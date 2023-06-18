800a6861ac8817d5ede27769af962ee7021d2d8f
Tung <dothanhtungnb602@gmail.com>
2023-06-18 15:39:13 +0700

    complete entity

--- app/src/main/java/com/example/fooddeliveryapp/data/db/entities/Order.java
+++ app/src/main/java/com/example/fooddeliveryapp/data/db/entities/Order.java
@@ -1,4 +1,18 @@
 package com.example.fooddeliveryapp.data.db.entities;
 
+import androidx.room.Entity;
+import androidx.room.ForeignKey;
+
+import java.util.Date;
+
+@Entity(tableName = "orders",
+        foreignKeys = {
+                @ForeignKey(entity = User.class, parentColumns = "id", childColumns = "userId", onDelete = ForeignKey.CASCADE)
+        })
 public class Order {
+    public int id;
+    public Date orderDate;
+    public int totalCost;
+    public String status;
+    public int userId;
 }
