b2a00744c31b661fe8c7b028ffef8b83b948cd48
hoang001359 <hoang001359@gmail.com>
2023-06-20 22:03:46 +0700

    favorite start

--- app/src/main/java/com/example/fooddeliveryapp/ui/food/FoodDetailsFragment.java
+++ app/src/main/java/com/example/fooddeliveryapp/ui/food/FoodDetailsFragment.java
@@ -21,12 +21,18 @@ import com.denzcoskun.imageslider.models.SlideModel;
 import com.example.fooddeliveryapp.MainActivity;
 import com.example.fooddeliveryapp.R;
 import com.example.fooddeliveryapp.data.db.AppDatabase;
+import com.example.fooddeliveryapp.data.db.entities.Favorite;
 import com.example.fooddeliveryapp.data.db.entities.Food;
 import com.example.fooddeliveryapp.data.db.entities.FoodImage;
 import com.example.fooddeliveryapp.data.db.entities.Restaurant;
+import com.example.fooddeliveryapp.data.db.entities.User;
+import com.example.fooddeliveryapp.data.repositories.FavoriteRepository;
 import com.example.fooddeliveryapp.data.repositories.FoodRepository;
+import com.example.fooddeliveryapp.data.repositories.UserRepository;
 import com.example.fooddeliveryapp.databinding.FragmentFoodDetailsBinding;
 import com.denzcoskun.imageslider.constants.ScaleTypes;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import org.w3c.dom.Text;
 
@@ -49,6 +55,15 @@ public class FoodDetailsFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         MainActivity.hideNavView();
+
+
+        AppDatabase database = AppDatabase.getDatabase(requireActivity());
+//        UserRepository userRepository = new UserRepository(database);
+//        FirebaseUser userFirebase = FirebaseAuth.getInstance().getCurrentUser();
+//        String email = userFirebase.getEmail();
+//        User user = userRepository.getUserByEmail(email);
+
+
         binding = FragmentFoodDetailsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
         foodRepository = new FoodRepository(AppDatabase.getDatabase(requireActivity()));
@@ -104,8 +119,14 @@ public class FoodDetailsFragment extends Fragment {
 //            Implement add to cart
             Navigation.findNavController(binding.getRoot()).navigate(R.id.action_foodDetailsFragment_to_navigation_cart);
         });
-
+        binding.toggleButton.setOnClickListener(v->{
+            FavoriteRepository favoriteRepository = new FavoriteRepository(database);
+            if(binding.toggleButton.isChecked()){
+                favoriteRepository.insertFavorite(food.id);
+            }else {
+                //Chưa làm được chức năng bỏ thích
+            }
+        });
         return root;
     }
-    
 }
